b509347a9198fc7c015f35df518a05542bda8069
Kreuvf <github@kreuvf.de>
2015-08-09 23:07:24 +0200

    * Enable usage of filenames which contain spaces * Add my name to the authors

--- pdf2png2pdf/pdf2png2pdf.sh
+++ pdf2png2pdf/pdf2png2pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Author(s): Toshiki Ishii
+# Author(s): Toshiki Ishii, Steven "Kreuvf" Koenig
 # Initial publication: 2015-06-26
 # http://chem.toshiki.de/2015/06/pdf-als-bild-rastern-und-wieder-zu-einer-pdf-datei-zusammenfuegen/
 # Licence(s):
@@ -10,7 +10,7 @@
 
 # Get information on PDF
 echo "Reading page information for $1"
-mapfile -t pageinfo < <(identify -density 300 $1)
+mapfile -t pageinfo < <(identify -density 300 "$1")
 pagecount=${#pageinfo[@]}
 echo "    -> $pagecount pages"
 
@@ -19,30 +19,37 @@ if [ $pagecount -gt 0 ]
 then
 
 # Make temporary directory
-filename=$(cut -d'/' -f$(($(grep -o "/" <<< $1 | wc -l)+1)) <<< $1 | cut -d'.' -f1)
+filename=$(cut -d'/' -f$(($(grep -o "/" <<< "$1" | wc -l)+1)) <<< "$1" | cut -d'.' -f1)
 tmppath="/tmp/pngpdf/$filename/"
-mkdir "/tmp/pngpdf/"
-mkdir "$tmppath"
+mkdir -p "$tmppath"
 
 # Render pages as PNG
 echo "Rendering PDF pages as PNG"
 curr=1
 files=""
+
+spaces=$(grep -o " " <<< "$1" | wc -l)
+
+declare -a filearray
+filearray=($files)
+
 while [ $curr -le $pagecount ]
 do
+	offset=$(expr $spaces + 2)
 	currpageinfo=(${pageinfo[$((curr-1))]})
-	currpagewidth=$(cut -d "x" -f 1 <<< ${currpageinfo[2]})
-	currpageheight=$(cut -d "x" -f 2 <<< ${currpageinfo[2]})
+	currpagewidth=$(cut -d "x" -f 1 <<< ${currpageinfo[$offset]})
+	currpageheight=$(cut -d "x" -f 2 <<< ${currpageinfo[$offset]})
 	echo -n "    -> page $curr of $pagecount (${currpagewidth}x${currpageheight}) ..."
 	convert -density 300 "$1"[$((curr-1))] "$tmppath"$curr.png
-	files="$files $tmppath$curr".png
+	newfiles="$tmppath$curr.png"
+	filearray[$curr-1]="$newfiles"
 	curr=$((curr+1))
 	echo " done"
 done
 
 # Combine to single PDF
 echo "Combining PNG images to single PDF"
-convert -monitor $files "$filename"_png.pdf
+convert -monitor "${filearray[@]}" "$filename"_png.pdf
 
 # Clean up
 echo "Deleting temporary files"
