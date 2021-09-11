76fbed1811788d0a2d86959e7ccde1c25e5850bb
Kreuvf <github@kreuvf.de>
2021-09-11 16:10:44 +0200

    * Accept space or equal sign

--- git/post-pull-fix2.sh
+++ git/post-pull-fix2.sh
@@ -5,7 +5,7 @@
 # Kick that out with core.autocrlf=input
 # Use in root dir of git repository
 # Fixes some newline-related weirdness
-sed -r -i -e '/^\* text=auto$/d' .gitattributes
+sed -r -i -e '/^\* text[ =]auto$/d' .gitattributes
 git status
 git checkout -- .gitattributes
 # Yeah, discarding the changes is ok, git is satisfied
